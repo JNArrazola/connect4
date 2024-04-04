eca3d7de970b3be6d79dc365368dd3b21699405d
Joshua <joshuanarrazola@gmail.com>
2024-04-03 22:01:59 -0600

    board was actually not working but now it is

--- app/src/main/java/connect4/Board.java
+++ app/src/main/java/connect4/Board.java
@@ -28,14 +28,15 @@ public class Board {
             System.out.print("-");
         
         System.out.println();
-        for(int i = 0; i < board.length; i++)
+        for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++)
                 if(j==0)
                     System.out.print("|" + board[i][j] + "|");
                 else 
                     System.out.print(board[i][j] + "|");
             System.out.println();
-        
+        }
+
         for(int i = 0; i < board[0].length*(2) + 1; i++)
             System.out.print("-");
         System.out.println();
--- app/src/main/java/connect4/Game.java
+++ app/src/main/java/connect4/Game.java
@@ -2,6 +2,8 @@ package connect4;
 
 
 public class Game {
+    private Board board = null;
+    
     public void start(){
         System.out.println("Ingresa el número de filas: ");
         int rows = utilities.validateNumberBoard();  
@@ -9,7 +11,24 @@ public class Game {
         System.out.println("Ingresa el número de columnas: ");
         int columns = utilities.validateNumberBoard();
 
-        Board board = new Board(rows, columns);
-        board.printBoard();
+        board = new Board(rows, columns);
+
+        do {
+            System.out.println("¿Qué modalidad desea jugar?");
+            System.out.println("1) Jugador contra jugador");
+            System.out.println("2) Jugador contra IA");
+            int opt = utilities.validateNumber();
+
+            switch (opt) {
+                case 1: // Jugador contra Jugador
+                    
+                    break;
+                case 2: // Jugador contra máquina
+                    
+                    break;
+                default:
+                    break;
+            }
+        } while (true);
     }
 }
